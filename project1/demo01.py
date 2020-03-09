b330ae8fcb15a8b1ef1f7c1aa443056fe887eecc
billchuan <1767618766@qq.com>
2020-03-09 15:10:14 +0800

    第七次课更新

--- project1/demo01.py
+++ project1/demo01.py
@@ -1,14 +1,11 @@
-import turtle
-turtle.pensize(5)
-turtle.pencolor("yellow")
-turtle.fillcolor("red")
-turtle.begin_fill()
-while True:
-    turtle.forward(200)
-    turtle.right(144)
-    if abs(turtle.pos()) < 1:
-        break
-turtle.end_fill()
-turtle.mainloop()
+print("小明去菜市场买菜")
+
+a=input("买了颗白菜花了:")
+
+b=input("买了条鱼花了:")
+
+c=float(a)+float(b)
+
+print("总共消费:%.2f"%c)
 
 
