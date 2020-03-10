9a6a6a629aca046f43c0e3f0ea739e807a6ee5af
billchuan <1767618766@qq.com>
2020-03-10 16:38:21 +0800

    第八、九次课更新

--- project1/demo03.py
+++ project1/demo03.py
@@ -1,11 +1,5 @@
-a = 123.1
-print("我要输出的整数值为：%d" % a)
-b = "sdfsdgg"
-print("我要输出的字符串的值为：%s" % b)
-c = 98.891
-print("我要输出的浮点数的值为：%.2f" % c)
-a = 123
-b = 321
-print("%d+%d=%d" % (a, b, a + b))
-a = 89.983
-print("我想输出一个百分数：%.2f%%" % a)
+i=1
+
+while i<=5:
+    print("hello world")
+    i=i+1
