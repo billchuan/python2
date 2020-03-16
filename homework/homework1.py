29957d8069c97f012edae3a7057a778aa9128e3b
billchuan <1767618766@qq.com>
2020-03-16 13:56:14 +0800

    第十次课作业

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,1 +1,18 @@
-dc
+
+'''for循环打印乘法表
+for i in range(1,9+1):
+    for j in range(1,i+1):
+        print("%d*%d=%d"%(i,j,i*j),end=" ")
+    print()
+'''
+
+'''while循环打印乘法表
+i=1
+while i<=9:
+    j=1
+    while j<=i:
+        print("%d*%d=%d"%(i,j,i*j),end=" ")
+        j+=1
+    print()
+    i+=1
+'''
