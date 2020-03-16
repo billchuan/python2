4419d5683860315f3f72ad778d8cdac7a1e4f643
billchuan <1767618766@qq.com>
2020-03-16 16:09:19 +0800

    第十次课更新

--- project1/demo05.py
+++ project1/demo05.py
@@ -1,24 +1,15 @@
-
-
-
-
-
-
-
-
 '''break关键字练习
 i=0
 while i<10:
-    print("i=%d"%i)
+    print("i=%d"%i,end=" ")
     i+=1
     if i==6:
         break
 '''
-
 ''' continue关键字练习
 i=0
 while i<10:
-     print("i=%d"%i)
+     print("i=%d"%i,end=" ")
      i+=1
      if i==6:
         i+=1
@@ -27,5 +18,21 @@ while i<10:
 '''for循环
 j="Hello python"
 for i  in j :
-    print(i)
+    print(i,end=" ")
 '''
+'''
+for i in range(0,5):
+    print(i,end=" ")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
