320e8771c01c8d133935f449e3288f437db648df
billchuan <1767618766@qq.com>
2020-03-16 13:34:35 +0800

    第十次课更新

---
+++ project1/demo05.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''break关键字练习
+i=0
+while i<10:
+    print("i=%d"%i)
+    i+=1
+    if i==6:
+        break
+'''
+
+''' continue关键字练习
+i=0
+while i<10:
+     print("i=%d"%i)
+     i+=1
+     if i==6:
+        i+=1
+        continue
+'''
