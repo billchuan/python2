7350e808ea6f18cd36df564c38d1799e0c49f875
billchuan <1767618766@qq.com>
2020-03-17 17:31:49 +0800

    第十二次课更新

--- project1/demo07.py
+++ project1/demo07.py
@@ -1,15 +1,29 @@
+# 封装参数练习
+def hello_world(s1,s2):
+    print(s1)
+    print(s2)
+
+
+hello_world(1,2)
+
 """无参函数练习
+
+
 def printNum(max):
-    for i in range(1,max):
-        print("i=%d"%i,end=" ")
+    for i in range(1, max):
+        print("i=%d" % i, end=" ")
+
 
 printNum(5)
 """
-
 '''参数返回函数练习
-def manji(width,height):
-    return width*height
 
-w=4;h=5
-print("宽=",w,"长=",h,"面积=",manji(w,h))
+
+def area(width, height):
+    return width * height
+
+
+w = 4;
+h = 5
+print("宽=", w, "长=", h, "面积=", area(w, h))
 '''
