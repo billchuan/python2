4de6c0862aad6171aed54d8a8bd974b3c608736d
billchuan <1767618766@qq.com>
2020-03-17 14:22:44 +0800

    第十二次课更新

---
+++ project1/demo07.py
@@ -0,0 +1,15 @@
+"""无参函数练习
+def printNum(max):
+    for i in range(1,max):
+        print("i=%d"%i,end=" ")
+
+printNum(5)
+"""
+
+'''参数返回函数练习
+def manji(width,height):
+    return width*height
+
+w=4;h=5
+print("宽=",w,"长=",h,"面积=",manji(w,h))
+'''
