e106e7c17ad59064b1329e29e81bde68e71e9d84
billchuan <1767618766@qq.com>
2020-03-23 20:42:19 +0800

    第十五次课作业

--- homework/homework2.py
+++ homework/homework2.py
@@ -3,7 +3,7 @@
 
 def print_num(num):
     if num == 4:
-        print("达到最里层文件夹，找到机密文件。开始返回")
+        print("到达最里层文件夹，找到机密文件。开始返回")
         return
     print("进入第%d层文件夹" % num)
     print_num(num + 1)
