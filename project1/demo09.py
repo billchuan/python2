535971328547fe10604f9ecdf8179c4bc4be7c7f
billchuan <1767618766@qq.com>
2020-03-24 16:29:47 +0800

    第十四次课学习更新

--- project1/demo09.py
+++ project1/demo09.py
@@ -1,1 +1,33 @@
-# 3月24日上课学习代码
+# 3月24日第十四次课上课代码
+
+
+# 局部变量练习
+# def method_1():
+#     name = "张三"
+#     print(name)
+#
+#
+# method_1()
+# print(name)# 无法调用方法内部的变量
+
+# 全局变量练习
+# name = "张三"
+#
+#
+# def method_1():
+#     """全局变量方法内部调用"""
+#     print(name, end=" ")
+#     print("这是方法二")
+#
+#
+# def method_2():
+#     """全局变量方法内部调用"""
+#     name = "lisi"
+#     print(name, end=" ")
+#     print("这是方法二")
+#
+#
+# print(name)
+# method_1()
+# method_2()
+
