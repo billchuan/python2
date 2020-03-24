66ab6ca11b3cc545d80f016f5eb8f349088bb0c0
billchuan <1767618766@qq.com>
2020-03-24 17:58:16 +0800

    第十五次课学习更新

--- project1/demo10.py
+++ project1/demo10.py
@@ -1,1 +1,41 @@
 # 3月24日第十五次课上课代码
+
+# 全局变量修改
+# name = "zhangsan"
+#
+#
+# def method_1():
+#     global name
+#     name = "lisi"
+#     print(name)
+#
+#
+# method_1()
+# print(name)
+
+
+# 函数嵌套
+# 在method_1中调用method_2
+# def method_2():
+#     print("2" * 50)
+#
+#
+# def method_1():
+#     print("1" * 50)
+#     method_2()
+#     print("1" * 50)
+#
+#
+# method_1()
+
+
+# 函数递归
+def print_num(num):
+    if num == 4:
+        return
+    print(num)
+    print_num(num + 1)
+    print(num)
+
+
+print_num(1)
