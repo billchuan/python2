4f9b9fa599c452c3794277f7d1f70d9c01ab9a17
billchuan <1767618766@qq.com>
2020-04-07 15:05:09 +0800

    4月7日更新

--- project2/study1.py
+++ project2/study1.py
@@ -1,4 +1,28 @@
 # 4月7日五、六节课学习代码
 
 
+# java的数组
+# int[]=arr={1,2,3,4,5}
+
+# python数组
+
+# 普通元组的创建
+type_info = ("zs", 18, 1.71)
+# print(type(type_info))
+
+# 空元组
+tuple_empty = ()
+# print(type(tuple_empty))
+
+# 包含一个元素的元组
+tuple_one = (18)
+# print(type(tuple_one))
+
+# 取值
+tuple_1 = ("张三", "李四", 123, "王五")
+# print(tuple_1[2])
+
+# 取索引号
+print(tuple_1.index("王五"))
+
 
