ccbe2c4240756c036c82bc8a392ae44fe38a0460
billchuan <1767618766@qq.com>
2020-04-07 17:07:03 +0800

    4月7日更新

--- project2/study2.py
+++ project2/study2.py
@@ -23,9 +23,13 @@ name_list1 = ["zs", "ls", 12, "zs"]
 
 # 列表的增加
 # append插入到结尾
-name_list1.append("王麻子")
+# name_list1.append("王麻子")
 # print(name_list1)
 # 某个位置插入
-name_list1.insert(1, "刘能")
+# print(name_list1)
+# name_list1.insert(3, "刘能")
+# print(name_list1)
+# extend将某个列表的所有数据追加到尾部
+name_list2 = [1, 2, 3, 4]
+name_list1.extend(name_list2)
 print(name_list1)
-
