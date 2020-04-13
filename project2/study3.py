b97b61ff0f226eaa229908840878758733478215
billchuan <1767618766@qq.com>
2020-04-13 16:04:59 +0800

    4月13日更新

--- project2/study3.py
+++ project2/study3.py
@@ -1,2 +1,42 @@
 # 4月13日学习
 
+# str1 = "111122223333"
+# str2 = "11\t2\t3"
+# print(str1)
+# print(str2)
+
+# 查找
+# startswith()以第一个字符开始查找
+# str1 = "helloworld"
+# r = str1.startswith("ell")
+# print(r)
+
+# 判断以什么字符为结尾
+# str1 = "helloworld"
+# r = str1.endswith("rl")
+# print(r)
+
+# 查找整个字符串是否包含指定字符串，包含返回索引,不包含返回-1
+# str1 = "helloworld"
+# r = str1.find("wod")
+# print(r)
+
+# 替换
+# str1 = "21810zhangsan"
+# r = str1.replace("21810", "21809")
+# print(r)
+
+# 拆分字符串,不写默认分隔符
+# s1 = "zs ls ww ln"
+# s1_list = s1.split()
+# print(s1_list)
+
+# 习题1
+# a = "hao,hao,xue,xi,tian,tian,xiang,shang"
+# a1 = a.replace(",", "-")
+# print(a1)
+
+# 习题2
+# a = "hao-hao-xue-xi-tian-tian-xiang-shang"
+# a1 = a.split("-")
+# print(a1)
