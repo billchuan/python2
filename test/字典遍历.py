c85eb66d0765a692f508e607370eed64078e6c11
billchuan <1767618766@qq.com>
2020-04-13 14:49:54 +0800

    4月13日更新

---
+++ test/字典遍历.py
@@ -0,0 +1,12 @@
+# 4月13日习题2
+info_divt1 = {"name": "zhangsan", "age": 18, "addr": "guizhou"}
+info_divt2 = {"age": 20, "tel": 16899168}
+info_divt1.update(info_divt2)
+print(info_divt1)
+
+# 4月13日习题1
+a = {}
+a["姓名"] = "张三"
+a["班级"] = 2186
+a["学号"] = 666
+print(a)
