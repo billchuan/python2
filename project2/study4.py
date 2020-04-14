2b89971c82c014e43a834ea0566cbf918e7e77cc
billchuan <1767618766@qq.com>
2020-04-14 15:44:12 +0800

    4月14日更新

---
+++ project2/study4.py
@@ -0,0 +1,57 @@
+# 4月14日学习
+
+# 习题1
+# a = set(["h", "e", "l", "o"])
+# print(a)
+
+# 习题2
+# s = {"zs", "ls"}
+# s.update(["ww"], ["ln"], [2], [3])
+# print(s)
+
+# 字典  dictionary
+# dic_1 = {"name": "zs",
+#          "sex": "男",
+#          "age": 18}
+# print(dic_1)
+
+# 取值
+# v1=dic_1["age"]
+# print(v1)
+
+# 增加
+# dic_1["height"] = 175
+# print(dic_1)
+
+# 修改
+# dic_1["age"] = 19
+# print(dic_1)
+
+# 删除
+# dic_1.pop("age")
+# print(dic_1)
+
+# 查看字典长度
+# print(len(dic_1))
+
+# 合并
+# dic_2 = {"height": 175, "banji": 21810}
+# dic_1.update(dic_2)
+# print(dic_1)
+
+
+# 清空
+# print(dic_1)
+# dic_1.clear()
+# print(dic_1)
+
+# 字典的遍历
+# for i in dic_1:
+#     print("%s:%s" % (i, dic_1[i]))
+
+# 字典和列表的联合应用
+dic_1 = {"name": "zs","sex": "男","age": 18}
+dic_2 = {"name": "ls","sex": "男","age": 17}
+dic_3 = {"name": "ww","sex": "男","age": 16}
+
+
