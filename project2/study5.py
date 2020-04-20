6473d861cdb7d91798f838b99341ea2c132cb5a2
billchuan <1767618766@qq.com>
2020-04-20 13:37:56 +0800

    4月20日更新

--- project2/study5.py
+++ project2/study5.py
@@ -1,1 +1,30 @@
+# # 4月20日学习
+# # 异常捕获完整语法
+# try:
+#     # 尝试执行的代码
+#     pass
+# except 错误类型1:
+#     # 针对错误类型1，对应的代码处理
+#     pass
+# except 错误类型2:
+#     # 针对错误类型2，对应的代码处理
+#     pass
+# except （错误类型3, 错误类型4):
+# # 针对错误类型3和4，对应的代码处理
+#     pass
+# except Exception as result:
+#     # 打印错误信息
+#     print(result)
+# else:
+#     # 没有异常才会执行的代码
+#     pass
+# finally:
+#     # 无论是否有异常，都会执行的代码
+#     print("无论是否有异常，都会执行的代码")
 
+# 习题1
+try:
+    tup_1 = (1, 2, 3)
+    tup_1[1] = 6
+except TypeError:
+    print("类型错误，元组中的元素不支持修改")
