297ed182ff69480c873024e54b4be98a20c4bf50
billchuan <1767618766@qq.com>
2020-04-21 15:12:09 +0800

    4月21日更新

--- project2/study6.py
+++ project2/study6.py
@@ -1,1 +1,31 @@
 # 4月21日五、六节学习
+# 习题1
+# file = open("C:\\Users\\Lenovo\\Desktop\\myinfo.txt",encoding="UTF-8",errors="ignore")
+# while True:
+#     text=file.readline()
+#     if not text:
+#         print("没有读取到内容")
+#         break
+#     print(text,end=" ")
+# file.close()
+
+# 习题2
+# file = open("C:\\Users\\Lenovo\\Desktop\\myinfo.txt", "a", encoding="UTF-8", errors="ignore")
+# try:
+#     file.write(" i am chinese")
+#     print("写入成功")
+# except Exception as result:
+#     print("写入失败%s" % result)
+# file.close()
+
+# 文件的读取
+
+# 打开文件
+# file_1 = open("C:\\Users\\Lenovo\\Desktop\\myinfo.txt", encoding="UTF-8")
+# # 读取文件
+# text_1 = file_1.read()
+# # 打印内容
+# print(text_1)
+# # 关闭文件
+# file_1.close()
+
--- project2/study7.py
+++ project2/study7.py
@@ -1,1 +1,24 @@
 # 4月21日七、八节学习
+# 习题1
+
+
+# class Tank:
+#     def fire(self):
+#         print("坦克发射炮弹")
+#
+#     def run(self):
+#         print("坦克开始行使")
+
+
+# 习题2
+# class Dog:
+#     def sleep(self):
+#         print("小狗正在睡觉")
+#
+#     def bark(self):
+#         print("小狗正在汪汪叫")
+#
+#
+# dog = Dog()
+# dog.sleep()
+# dog.bark()
