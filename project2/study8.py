91ae7e34d1ca9dc3463fd6364a6f03ebe64495b8
billchuan <1767618766@qq.com>
2020-04-26 13:45:31 +0800

    4月26日更新

---
+++ project2/study8.py
@@ -0,0 +1,23 @@
+# 4月26日学习
+# 习题
+
+
+# class Car:
+#     def turn_left(self):
+#         print("车向左转")
+#
+#     def turn_right(self):
+#         print("车向右转")
+#
+#
+# class WarCar(Car):
+#     def fire(self):
+#         print("战车开火")
+#
+#
+# car = WarCar()
+# car.turn_left()
+# car.turn_right()
+# car.fire()
+
+
