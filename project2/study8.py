5e3eae7c7f4633abf3f62ab0e3c5442b8441afe8
billchuan <1767618766@qq.com>
2020-04-26 16:04:29 +0800

    4月26日更新

--- project2/study8.py
+++ project2/study8.py
@@ -21,21 +21,40 @@
 # car.fire()
 
 # 继承的传递性
-class Animal:
-    def eat(self):
-        print("吃饭")
+# class Animal:
+#     def eat(self):
+#         print("吃饭")
+#
+#     def drink(self):
+#         print("喝水")
+#
+#     def run(self):
+#         print("奔跑")
+#
+#     def sleep(self):
+#         print("睡觉")
+#
+#
+# class Dog(Animal):
+#     def bark(self):
+#         print("汪汪叫")
+#
+#
+# class XTDog(Dog):
+#     def fly(self):
+#         print("飞起")
+#
+#
+# xt = XTDog()
+# xt.eat()
+# xt.drink()
+# xt.sleep()
+# xt.run()
+# xt.bark()
+# xt.fly()
 
-    def drink(self):
-        print("喝水")
-
-    def run(self):
-        print("奔跑")
-
-    def sleep(self):
-        print("睡觉")
-
-
-class Dog(Animal):
+# 对父类的拓展
+class Dog:
     def bark(self):
         print("汪汪叫")
 
@@ -44,11 +63,11 @@ class XTDog(Dog):
     def fly(self):
         print("飞起")
 
+    def bark(self):
+        print("在天上汪汪叫")
+        super().bark()
+        print("￥￥%￥￥%&$&$&")
+
 
 xt = XTDog()
-xt.eat()
-xt.drink()
-xt.sleep()
-xt.run()
 xt.bark()
-xt.fly()
