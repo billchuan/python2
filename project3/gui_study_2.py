75e9535fd215b2e51d465f102029b39a0a694286
billchuan <1767618766@qq.com>
2020-05-09 18:09:58 +0800

    5月9日更新

--- project3/gui_study_2.py
+++ project3/gui_study_2.py
@@ -1,26 +1,33 @@
 # 5月9日七、八节课学习
+# 位置坐标网格
+# import tkinter as tk
+#
+# top = tk.Tk()
+# top.title("我的窗口")
+# top.geometry("300x500")
+# top.resizable(True, True)  # 宽、高是否能改变
+#
+# # 第1个按钮
+# text_str = tk.StringVar()
+# text_str.set("按钮1")
+# btn1 = tk.Button(top, textvariable=text_str)
+# btn1.place(x=50, y=50, width=50, height=100)
+# top.mainloop()
 
+# label组件
+import tkinter as tk
 
+top = tk.Tk()
+top.title("我的窗口")
+top.geometry("300x200")
+top.resizable(False, False)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 创建标签组件
+label_bar = tk.StringVar()
+label_bar.set("请输入你的姓名：")
+label_obj = tk.Label(top, textvariable=label_bar)
+label_obj.pack()
+top.mainloop()
 
 # 习题一
 # import tkinter as tk
