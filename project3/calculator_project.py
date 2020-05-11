97e9531773a7897b3111830c232ec0b0d6138ae2
billchuan <1767618766@qq.com>
2020-05-11 16:09:45 +0800

    5月11日更新

--- project3/calculator_project.py
+++ project3/calculator_project.py
@@ -1,14 +1,17 @@
+# 实现简单计算器的图形界面
 import tkinter as tk
 
 win = tk.Tk()
-win.title("整数计算器")
+win.title("整数计算器")  # 标题
 win.geometry("240x264")  # 宽x高
-win.resizable(False, False)
+win.resizable(False, False) # 设置宽、高是否可以改变
+
 # 文本输入框
 result = tk.StringVar()
+result.set("0")
 ent = tk.Entry(win, textvariable=result)
 ent.place(x=0, y=0, width=240, height=24)
-result.set("0")
+
 
 # 创建键盘区
 txts = ["1", "2", "3", "+",
