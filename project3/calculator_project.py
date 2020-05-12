3abd67841789e55c872c361e31966c921b559910
billchuan <1767618766@qq.com>
2020-05-12 14:03:27 +0800

    5月12日更新

--- project3/calculator_project.py
+++ project3/calculator_project.py
@@ -4,7 +4,7 @@ import tkinter as tk
 win = tk.Tk()
 win.title("整数计算器")  # 标题
 win.geometry("240x264")  # 宽x高
-win.resizable(False, False) # 设置宽、高是否可以改变
+win.resizable(False, False)  # 设置宽、高是否可以改变
 
 # 文本输入框
 result = tk.StringVar()
@@ -12,17 +12,47 @@ result.set("0")
 ent = tk.Entry(win, textvariable=result)
 ent.place(x=0, y=0, width=240, height=24)
 
-
 # 创建键盘区
 txts = ["1", "2", "3", "+",
         "4", "5", "6", "-",
         "7", "8", "9", "*",
         "C", "0", "=", "/"]
 
+# 添加功能
+opt = 0  # 获取输入的数字
+ope = ""  # 保存操作符
+save = 0  # 保存前一个运算数字
+
+def calc(event):
+    global opt
+    global ope
+    global save
+    if event.widget["text"] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        opt = opt * 10 + int(event.widget["text"])
+        result.set(str(opt))
+    elif event.widget["text"] in ["+", "-", "*", "/"]:
+        save = opt
+        ope = event.widget["text"]
+        opt = 0
+        result.set("0")
+    elif event.widget["text"] == "C":
+        opt = 0
+        result.set("0")
+    elif event.widget["text"] == "=":
+        if ope == "+":
+            result.set(str(save + opt))
+        if ope == "-":
+            result.set(str(save - opt))
+        if ope == "*":
+            result.set(str(save * opt))
+        if ope == "/":
+            result.set(str(save / opt))
+
+
 for i in range(16):
     strBtn = tk.StringVar()
     strBtn.set(txts[i])
     btn = tk.Button(win, textvariable=strBtn)
     btn.place(x=i % 4 * 60, y=i // 4 * 60 + 24, width=60, height=60)
-
+    btn.bind("<Button-1>", calc)
 tk.mainloop()
