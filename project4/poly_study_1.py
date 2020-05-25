20ac575c7d2dc634587fbcf55edffbb7db159a92
billchuan <1767618766@qq.com>
2020-05-25 12:50:51 +0800

    5月25日更新

---
+++ project4/poly_study_1.py
@@ -0,0 +1,26 @@
+# 5月25日五、六节课学习
+
+# 习题二 多态练习
+# class Car(object):
+#     def run(self):
+#         print("车在行驶")
+#
+#
+# class BMW(Car):
+#     def run(self):
+#         print("宝马在行驶")
+#
+#
+# class QiRui(Car):
+#     def run(self):
+#         print("奇瑞在行驶")
+#
+#
+# class Person(object):
+#     def drive(self, car):
+#         car.run()
+#
+#
+# p = Person()
+# p.drive(BMW())
+# p.drive(QiRui())
